c06a94e96006159659c2d725b143d7ef0f109830
MixidFinder <mixidfinder@gmail.com>
2025-04-04 19:09:39 +0300

    db_service added

---
+++ symptosay/services/db_service.py
@@ -0,0 +1,40 @@
+import logging
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
+from . import connection
+
+load_dotenv(find_dotenv())
+
+logger = logging.getLogger(__name__)
+
+DATABASE_SERVICE_URL = os.getenv("DATABASE_SERVICE_URL")
+
+
+async def get_symptom_by_name(symptom: str):
+    await connection.request_service(method="get", url=f"{DATABASE_SERVICE_URL}/api/symptoms/{symptom}")
+
+
+async def get_all_symptoms():
+    await connection.request_service(method="get", url=f"{DATABASE_SERVICE_URL}/api/symptoms")
+
+
+async def add_symptoms(symptoms: list[dict]):
+    await connection.request_service(method="put", url=f"{DATABASE_SERVICE_URL}/api/symptoms", data=symptoms)
+
+
+async def add_diseases(diseases: list[dict]):
+    await connection.request_service(method="post", url=f"{DATABASE_SERVICE_URL}/api/diseases", data=diseases)
+
+
+async def get_all_diseases():
+    await connection.request_service(method="get", url=f"{DATABASE_SERVICE_URL}/api/diseases")
+
+
+async def get_disease_by_name(disease: str):
+    await connection.request_service(method="get", url=f"{DATABASE_SERVICE_URL}/api/diseases/{disease}")
+
+
+async def del_symptoms(symptoms: list[dict]):
+    await connection.request_service(method="delete", url=f"{DATABASE_SERVICE_URL}/api/symptoms", data=symptoms)
