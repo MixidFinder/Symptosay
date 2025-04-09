964fefd7407a865dc688c05c01f9cc9264aed041
MixidFinder <mixidfinder@gmail.com>
2025-04-09 19:09:43 +0300

    fix: 404 error exception

--- symptosay/middlewares/user_middleware.py
+++ symptosay/middlewares/user_middleware.py
@@ -4,6 +4,7 @@ from typing import Any, Callable
 
 from aiogram import BaseMiddleware
 from aiogram.types import CallbackQuery, Message
+from httpx import HTTPStatusError, codes
 from services.adm_user_service import get_user_by_id, register_user
 
 
@@ -16,17 +17,19 @@ class UserDataMiddleware(BaseMiddleware):
     ) -> Awaitable[Any]:
         user_id = event.from_user.id
         username = event.from_user.username
-
-        user_data = await get_user_by_id(user_id)
-
         admins = os.getenv("ADMINS", "").split(",")
         is_admin = str(user_id) in admins
 
-        if not user_data:
-            new_user = {"user_id": user_id, "username": username, "is_admin": is_admin}
-            user_data = await register_user(new_user)
-            if user_data is None:
-                user_data = {"user_id": user_id, "username": username, "is_admin": is_admin}
+        try:
+            user_data = await get_user_by_id(user_id)
+        except HTTPStatusError as e:
+            if e.response.status_code == codes.NOT_FOUND:
+                new_user = {"user_id": user_id, "username": username, "is_admin": is_admin}
+                user_data = await register_user(new_user)
+                if user_data is None:
+                    user_data = {"user_id": user_id, "username": username, "is_admin": is_admin}
+            else:
+                await event.answer("Сервис недоступен, попробуйте позже")
 
         if "is_admin" not in user_data:
             user_data["is_admin"] = is_admin
